358f47eec812d96156b7864948d980a80b70bfa5
nico24687 <theonlynico@hotmail.com>
2017-09-06 19:50:58 +0000

    Fist sinatra server

---
+++ web_guesser.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+
+get '/' do 
+    "Hello world"
+end 
+
+
+
+
+
+
+
+
+
+
+
+#note when running sinatra from cloud 9 you need to use: ruby <lolnotrealserverfile.rb> -o $IP -p $PORT
+#more info at https://gist.github.com/caseywatts/2f5f8bd2fdf583dc757a
